c4d1b0a50f19dabb04d18c2d5311759633e983e3
Takayuki Kaisen <ksn0215@gmail.com>
2019-02-28 05:33:06 +0900

    first commit

---
+++ assignments/assignment1/app.js
@@ -0,0 +1,37 @@
+(function () {
+'use strict';
+
+angular.module('MsgApp', [])
+.controller('MsgController', MsgController);
+
+MsgController.$inject = ['$scope'];
+function MsgController($scope) {
+  $scope.item = "";
+  $scope.itemCount = 0
+  $scope.textMessage = function(){
+    if ($scope.itemCount == 0){
+    	return "Please enter data first";
+    }
+    else if($scope.itemCount > 3){
+    	return "Too much!";
+    }
+    else{
+    	return "Enjoy!"
+    }
+  };
+
+  $scope.checkIfTooMuch = function () {
+  	var totalItemNumbers = calculatNumberOfItems($scope.item); 
+    $scope.itemCount = totalItemNumbers;
+    //console.log($scope.itemCount);
+  };
+
+  function calculatNumberOfItems(string){
+   	var totalStringCount = 0;
+   	var stringArray = string.split(',');
+    var totalItemCount = stringArray.length;
+   	return totalItemCount;
+   };
+}
+
+})();
